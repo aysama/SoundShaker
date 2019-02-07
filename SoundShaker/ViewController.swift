22bef1a9c7aa2401ecf32381fda540bd6e66fb69
Ayoola Samagbeyi <ayoolasamagbeyi@yahoo.com>
2019-02-07 16:24:31 +0100

    Initial Commit

---
+++ SoundShaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundShaker
+//
+//  Created by Olatunji Fagbore on 07/02/2019.
+//  Copyright © 2019 Softskan Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
